e9af22dbd5edfb5f245901dfb0ab14e06a1a843c
PinappleBuddy <steve1990@gmx.net>
2018-02-14 21:59:14 +0100

    Parser erstellt

---
+++ src/Parser.java
@@ -0,0 +1,12 @@
+public class Parser {
+
+    private static final char FRAKTION = '\u002f';
+    private static final char OPEN = '\u0028';
+    private static final char CLOSE = '\u0029';
+
+
+    public static Element parse(String input){
+        
+    }
+
+}
