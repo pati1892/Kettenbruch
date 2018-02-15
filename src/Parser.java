024a72ad7c62a9973dbfedeb8f909b3625edbec6
phenkelmann <phenkelmann@online.de>
2018-02-15 22:18:13 +0100

    Parser: removeBracket, parseInput

--- src/Parser.java
+++ src/Parser.java
@@ -2,7 +2,7 @@ import java.util.regex.Pattern;
 
 public class Parser {
 
-    private static final char FRAKTION = '\u002f';
+    private static final char FRACTION = '\u002f';
     private static final char OPEN = '\u0028';
     private static final char CLOSE = '\u0029';
 
@@ -11,12 +11,12 @@ public class Parser {
         if(input.length() - input.replaceAll(String.valueOf(OPEN),"").length() != input.length() - input.replaceAll(String.valueOf(CLOSE),"").length()){
             throw new IllegalArgumentException();
         }
-        if(Pattern.matches(FRAKTION + "{2,}", input)){
+        if(Pattern.matches(FRACTION + "{2,}", input)){
             throw new IllegalArgumentException("//");
         }
         input = input.trim();
 
-        if (!input.contains(String.valueOf(FRAKTION))) {
+        if (!input.contains(String.valueOf(FRACTION))) {
             Term neu = new Term(input);
             return neu;
         }
@@ -25,4 +25,32 @@ public class Parser {
         return null;
     }
 
+    private static Element parseInput(String input){
+        input = input.trim();
+        input = removeBracket(input);
+        if(input.contains(String.valueOf(FRACTION))){
+
+        }
+
+    }
+
+    private static String removeBracket(String input){
+        int counter = 0;
+        boolean remove = false;
+        for(int i = 0; i<input.length(); i++){
+            if(input.charAt(i) == OPEN) counter++;
+            else if(input.charAt(i) == CLOSE) counter --;
+            if(counter == 0){
+                if(i == input.length()-1){
+                    remove = true;
+                }
+            }
+        }
+        if(remove){
+            return removeBracket(input.substring(1, input.length()-1));
+        }
+        else{
+            return input;
+        }
+    }
 }
