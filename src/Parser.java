6037958170153aa22080a3a5fa6c146002a1a869
PinappleBuddy <steve1990@gmx.net>
2018-02-15 23:09:18 +0100

    erster test

--- src/Parser.java
+++ src/Parser.java
@@ -7,6 +7,7 @@ public class Parser {
     private static final char CLOSE = '\u0029';
 
 
+
     public static Element parse(String input){
         if(input.length() - input.replaceAll(String.valueOf(OPEN),"").length() != input.length() - input.replaceAll(String.valueOf(CLOSE),"").length()){
             throw new IllegalArgumentException();
@@ -14,34 +15,52 @@ public class Parser {
         if(Pattern.matches(FRACTION + "{2,}", input)){
             throw new IllegalArgumentException("//");
         }
+
+
         input = input.trim();
 
         if (!input.contains(String.valueOf(FRACTION))) {
-            Term neu = new Term(input);
-            return neu;
+            return new Term(input);
         }
 
-
-        return null;
-    }
-
-    private static Element parseInput(String input){
-        input = input.trim();
-
-        if (!input.contains(String.valueOf(FRACTION))) {
-            Term neu = new Term(input);
-            return neu;
-        }
         input = removeBracket(input);
 
         String[] fraction = findFraction(input);
-        if(fraction.length == 2){
-            return new Fraction(parseInput(fraction[0]), parseInput(fraction[1]));
+        if(fraction != null && fraction.length == 2){
+            return new Fraction(parse(fraction[0]), parse(fraction[1]));
         }
 
+        String[] operator = findOperator(input);
 
+        return new Operator(parse(operator[0]), operator[1], parse(operator[2]));
 
-        return null;
+    }
+
+    private static String[] findOperator(String input) {
+        int counter = 0;
+        String operant_1 = "";
+        String operant_2 = "";
+        String operator = "";
+
+        int index = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == OPEN) {
+                index = i;
+            } else if (input.charAt(i) == CLOSE) {
+                if (operant_1.equals(""))
+                    operant_1 = input.substring(index, i);
+                else
+                    operant_2 = input.substring(index, i);
+
+            }
+            operator = input.replaceAll(operant_1, "");
+            operator = operator.replaceAll(operant_2, "");
+            if (operant_2.equals("")) {
+                operant_2 = operant_1;
+                operant_1 = "";
+            }
+        }
+        return new String[]{operant_1, operator, operant_2};
     }
 
     private static String[] findFraction(String input){
@@ -55,6 +74,7 @@ public class Parser {
                 break;
             }
         }
+        if(index == -1) return null;
         String nenner = input.substring(0, index-1);
         String zaehler = input.substring(index+1, input.length()-1);
         return new String[]{nenner, zaehler};
